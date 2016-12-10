fe17451e239d66f316f78c909bc7debe5b9a0c07
xiangze <xiangze@gmail.com>
2016-12-11 07:54:48 +0900

    add observation

--- genLorenz.py
+++ genLorenz.py
@@ -1,3 +1,5 @@
+import math
+
 def dLorenz(x,a,b,c,dt):
     return [x[0]+a*(x[1]-x[0])*dt,
             x[1]+(x[0]*(b-x[2])-x[1])*dt,
@@ -12,11 +14,25 @@ def Lorenz(x0,T,a=10,b=28,c=2.66,dt=0.01):
         xs.append(x);
     return xs
 
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
 if __name__ == '__main__':
     import numpy as np 
+    from scipy.stats import bernoulli
+    obsnum=20
+
     x0=[0.2,0.2,0.1]
     out=Lorenz(x0,10000)
-    out=np.array(out).T
-    out=out[:,::10]
-    np.savetxt("data.csv",out.T,delimiter=",")    
+    out=np.array(out)
+    print out.shape
+    out=out[::10,:]
+#    out=out[::10,:]
+
+    obs=[]
+    for on in out:    
+        obs.append([bernoulli.rvs(sigmoid(on[0])) for j in xrange(obsnum)])
+    obs=np.array(obs)
+    np.savetxt("data.csv",obs,delimiter=",")    
 
