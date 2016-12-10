1ae0f962975603c46aeeba76aee072abd5816fad
xiangze <xiangze@gmail.com>
2016-12-11 07:59:57 +0900

    for 1-dimension and multitime observation,

--- run_rslds.py
+++ run_rslds.py
@@ -5,12 +5,12 @@ pystan.StanModel(file="RSLDS.stan")
 
 y=np.loadtxt("data.csv",delimiter=",")
 N=y.shape[0]
-M=y.shape[1]
+T=y.shape[1]
+M=3
 K=2
 
-data={"N":N,"M":M,"K":K,"y":d}
-
+data={"T":T,"N":N,"M":M,"K":K,"y":y}
 fit= smodel.sampling(data, iter=31000, warmup=1000, thin=10, chains=3, seed=71)
 res=fit.extract()
-   
+print res   
 
